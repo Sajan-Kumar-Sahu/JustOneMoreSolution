0836b25f3c258959a6c688846eb85f49d4ff629f
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2025-09-03 21:04:16 +0530

    Merge sort code implementation

---
+++ src/DSA_Problems/Sortings/Utility.java
@@ -0,0 +1,10 @@
+package DSA_Problems.Sortings;
+
+public class Utility {
+
+    public static void  printArray(int[]arr){
+       for(int num : arr){
+           System.out.print(num+" ");
+       }
+    }
+}
