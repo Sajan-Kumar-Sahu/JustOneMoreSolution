15bb5a59909be86539b1f171069cb90aaf432444
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2025-09-08 03:37:25 +0530

    Insertion sort implementation

---
+++ src/DSA_Problems/Sortings/InsertionSort.java
@@ -0,0 +1,23 @@
+package DSA_Problems.Sortings;
+
+public class InsertionSort {
+    public static void main(String[]args){
+        int[] arr = {5,4,10,1,6,2};
+        insertionSort(arr);
+        Utility.printArray(arr);
+    }
+
+    public static void insertionSort(int[] arr){
+        int temp;
+        int j;
+        for(int i=1;i<arr.length;i++){
+            temp = arr[i];
+            j = i-1;
+                while(j>=0 && arr[j]>temp){
+                    arr[j+1] = arr[j];
+                    j--;
+                }
+            arr[j+1] = temp;
+        }
+    }
+}
