7e3754965e5df064fc5ec91ae2a43767dc966357
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2025-09-09 05:18:28 +0530

    Reverse a string using stack

---
+++ src/DSA_Problems/Stacks/ReverseAString.java
@@ -0,0 +1,26 @@
+package DSA_Problems.Stacks;
+
+import java.util.Stack;
+
+public class ReverseAString {
+    public static void main(String[] args) {
+        String str = "Backbench_Dev";
+        String result = reverseString(str);
+        System.out.println(result);
+    }
+
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while (idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while (!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+}
